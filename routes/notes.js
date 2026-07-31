6edbe0039f1f28490f8848d0dcbf687ce31f301e
深町正聡 <masafuka815@gmail.com>
2026-08-01 02:08:08 +0900

    last update

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); //corsミドルウェア追加
+require('dotenv').config();
 
 const { MongoClient } =require('mongodb');
-const uri= "";
-;
+const uri= process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 router.get('/', async (req,res) => {
